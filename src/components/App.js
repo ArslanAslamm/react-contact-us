b12f82ad5c6d465a2064bcd5e316ce18ea3c4682
jiyasom <36759455+jiyasom@users.noreply.github.com>
2022-11-06 15:43:40 +0400

    working on localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 function App() {
+  const Local_Storage_Key = "contacts";
   const [contact, setContact] = useState([]);
 
   const newContact = (contactData) => {
-    alert(JSON.stringify(contactData));
-    setContact((old) => {
-      return [...old, contactData];
-    });
+    if (contact.find((data) => data.email === contactData.email)) {
+      alert("Email already Exist.");
+      return;
+    }
+    // setContact((old) => {
+    //   return [...old, contactData];
+    // });
+    setContact([...contact, contactData]);
   };
+
+  const deleteFunction = (data) => {
+    const remain = contact.filter((single) => single.email !== data);
+    setContact(remain);
+  };
+  useEffect(() => {
+    const getContacts = JSON.parse(localStorage.getItem(Local_Storage_Key));
+    setContact(getContacts);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem(Local_Storage_Key, JSON.stringify(contact));
+  }, [contact]);
   return (
     <div className="ui container">
       <Header />
       <AddContact newContact={newContact} />
-      <ContactList contacts={contact} />
+      <ContactList contacts={contact} deleteFunction={deleteFunction} />
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,7 +3,7 @@ import ContactCard from "./ContactCard";
 const ContactList = (props) => {
   const ContactsData = props.contacts.map((contact, i) => {
     return (
-        <ContactCard contact={ contact } key={i} />
+        <ContactCard contact={ contact } key={i} deleteFunction={props.deleteFunction} />
     );
   });
   return <div className="ui celled list">{ContactsData}</div>;
